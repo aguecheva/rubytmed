7bd8e16d4c42a3d87fe8b231139e4c260deed526
Agustin Echevarria <aguecheva@gmail.com>
2024-01-02 09:35:23 -0300

    creacion de pacientes y visualizacion de lista funcionales...

--- db/migrate/20231211193047_create_patients.rb
+++ db/migrate/20231211193047_create_patients.rb
@@ -4,23 +4,37 @@ class CreatePatients < ActiveRecord::Migration[7.1]
       #Datos filiales
       t.string :name
       t.string :lastname
-      t.integer :dni
-      t.date :birth_date
+      t.integer :dni 
+      t.date :birth_date 
 
-      #Datos de composicion familiar
+      #Datos de composicion familiar (hijos, pareja/s, padres, hermanos)
+      t.integer :children, :null => true
+      t.string :partner, :null => true 
+      t.string :parents, :null => true 
+      t.string :siblings, :null => true 
 
-      #Datos de contacto y localizacion 
+      #Datos de contacto y localizacion (direccion, telefono,)
+      t.string :address, :null => true
+      t.string :phone, :null => true
 
-      #Antecedentes familiares 
+      #Antecedentes familiares (enfermedades padecidas por los padres y hermanos)
+      t.text :family_history , :null => true
 
-      #Antecedentes personales
+      #Antecedentes personales (etiquetas con enfermeddes o grupos de enfermedades)
+      t.text :personal_history , :null => true
 
-      #Ocupacion
+      #Ocupacion (etiquetas con ocupacioens)
+      t.string :occupation, :null => true
 
-      #Habitos
+      #Habitos (alimentacion, ejercicio, consumo de sustancias, actividad sexual, )
+      t.string :diet, :null => true
+      t.string :exercise, :null => true
+      t.string :substance_use , :null => true
+      t.string :sexual_activity, :null => true
+      
 
-
-
+      #historial de consultorio/internaciones/etc... solo texto para registrar el motivo de consulta, enfermedad actual, diagnostico presuntivo y tratamiento aplicado o seguimiento del mismo
+      t.string :consult_record, :null => true
 
       t.timestamps
     end

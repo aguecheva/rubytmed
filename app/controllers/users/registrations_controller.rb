b204b20c7e4115b92a54c2542d8c462c3143220b
Agustin Echevarria <aguecheva@gmail.com>
2024-03-18 16:17:21 -0300

    añadiendo users a la base datos de devise, error user.blank

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # Sobrescribe las acciones que necesites aquí
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # En tu modelo de User
 class User < ApplicationRecord
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 devise :database_authenticatable, :registerable,

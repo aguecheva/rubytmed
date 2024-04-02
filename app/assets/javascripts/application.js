91c9fb51d9108b6cf4216efa3c6257c0cdba22bb
Agustin Echevarria <aguecheva@gmail.com>
2024-04-02 01:44:23 -0300

    cambio css y modo oscuro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,18 @@
-//= require jquery
-//= require jquery_ujs
+let toggle = document.getElementById("toggle");
+let label_toggle = document.getElementById("label_toggle");
 
-$().ready(() => {
-  console.log("grande lucho");
+toggle.addEventListener("change", (event) => {
+  let checked = event.target.checked;
+  document.body.classList.toggle('dark');
+  let links = document.querySelectorAll(".init a");
+
+  links.forEach((link) => {
+    if (checked) {
+      link.classList.add("dark");
+      label_toggle.innerHTML = '<i class="far fa-sun"></i>';
+    } else {
+      link.classList.remove("dark");
+      label_toggle.innerHTML = '<i class="far fa-moon"></i>';
+    }
+  });
 });

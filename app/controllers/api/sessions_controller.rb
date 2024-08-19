5ce11ecd10ac6003f73e0c5209a9654000b0b6ed
Agustin Echevarria <aguecheva@gmail.com>
2024-08-19 16:30:15 -0300

    API singup crerated

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Api::SessionsController < Api::BaseController
-  skip_before_action :authenticate_user_with_token
+  skip_before_action :authenticate_user_with_token, only: [:login, :signup]
 
   def login
     user = User.find_by(username: params[:username])
@@ -9,6 +9,17 @@ class Api::SessionsController < Api::BaseController
       render json: { token: token }
     else
       head :unauthorized
+    end
+  end
+
+
+  def signup
+    user = User.new(username: params[:username], password: params[:password])
+
+    if user.save
+      render json: { message: "User created" }.
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     post :login, to: 'sessions#login'
+    post :signup, to: 'sessions#signup'
   end
 end

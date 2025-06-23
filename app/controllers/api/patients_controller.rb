d8e9548ac1811f37c3fcbf3909df0f7a23b8ae5a
Agustin Echevarria <aguecheva@gmail.com>
2025-06-23 19:38:10 -0300

    creatin patient search succesfully

--- app/controllers/api/patients_controller.rb
+++ app/controllers/api/patients_controller.rb
@@ -1,56 +1,42 @@
 class Api::PatientsController < Api::BaseController
   def index
-    render status: 200,
-    ok: true,
-    json: {
-        data: Patient.all
-      }
+    puts "PARAMS: #{params.inspect}"
+
+    if params[:q]
+      @patients = Patient.where(
+        "name LIKE ? OR lastname LIKE ? OR dni LIKE ?",
+        "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%"
+      )
+    else
+      @patients = Patient.all
+    end
+
+    render status: 200, json: { data: @patients }
   end
 
   def show
     patient = Patient.find_by(id: params[:id])
 
     if patient.present?
-      render status: 200,
-        ok: true,
-        json: {
-        data: patient
-        }
+      render status: 200, json: { data: patient }
     else
-      head({  :status => 404,
-              :ok => false
-      })
+      head 404
     end
   end
 
   def create
     patient = Patient.new(patient_params)
 
-    if patient.save()
-      render status: 200,
-      ok: true,
-      json: {
-        data: patient
-      }
+    if patient.save
+      render status: 200, json: { data: patient }
     else
-      render status: 422,
-      ok: false,
-      json: {
-        errors: patient.errors.full_messages
-      }
+      render status: 422, json: { errors: patient.errors.full_messages }
     end
   end
+
+  private
+
   def patient_params
     params.permit(:name, :lastname, :dni, :birth_date)
   end
-
-  def index
-  if params[:q]
-    @patients = Patient.where("name LIKE ? OR lastname LIKE ? OR dni LIKE ?",
-                              "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%")
-  else
-    @patients = Patient.all
-  end
-
-  render status: 200, ok: true, json: { data: @patients }
 end
